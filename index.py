889f1bd0c7375acacf5751d381a4c36ed98a89c7
0xGingi <0xgingi@0xgingi.com>
2023-12-16 20:38:37 -0500

    modified:   index.py

--- index.py
+++ index.py
@@ -32,9 +32,9 @@ class YunaServer:
         self.app.route('/register', methods=['POST'])(self.handle_register)
         self.app.route('/check_login', methods=['GET'])(self.check_login)
 
-    def render_index(self):
-        return send_from_directory('.', 'index.html')
-
+    def run(self):
+        self.app.run(host='0.0.0.0', port=self.config["server"]["port"])
+        
     def handle_history_request(self):
         data = request.get_json()
         chat_id = data.get('chat')

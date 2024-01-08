813a49283e4a696115aa7bcd5f2da978e9bc870c
0xgingi <0xgingi@0xgingi.com>
2024-01-08 12:45:45 -0500

    modified:   menu.py 	modified:   requirements-amd.txt 	modified:   requirements-nvidia.txt 	modified:   requirements.txt

--- menu.py
+++ menu.py
@@ -8,9 +8,14 @@ except ImportError:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "pytermgui"])
     import pytermgui as ptg
 
+def _define_layout() -> ptg.Layout:
+    layout = ptg.Layout()
+    layout.add_slot("Body")
+    return layout
+
 windows = {}
-#title_label = ptg.Label(ptg.markup("[210 bold]========== Menu =========="))
 title_label = ptg.Label("[210 bold]========== Menu ==========")
+
 def info(event):
     os.system('clear')
     print("Welcome to Yuna Management Script!")
@@ -30,14 +35,16 @@ def start_yuna(event):
 
 def install_update_dependencies(event):
     windows['configure_gpu'] = ptg.Window(
-        ptg.Label("========== Installation... =========="),
+        ptg.Label("[210 bold]========== Install =========="),
         ptg.Button("CPU", onclick=install_cpu),
         ptg.Button("NVIDIA GPU", onclick=install_nvidia),
         ptg.Button("AMD GPU", onclick=install_amd),
         ptg.Button("Back", onclick=lambda event: manager.remove(windows['configure_gpu']))
     )
-    manager.add(windows['configure_gpu'])
-    manager.focus(windows['configure_gpu'])
+    with ptg.WindowManager() as manager:
+        manager.layout = _define_layout()
+        manager.add(windows['configure_gpu'])
+        manager.focus(windows['configure_gpu'])
 
 def install_cpu(event):
     print("Installing CPU dependencies...")
@@ -57,6 +64,7 @@ def install_nvidia(event):
 
 def install_amd(event):
     print("Installing AMD dependencies...")
+    subprocess.check_call("CT_HIPBLAS=1", [sys.executable, "-m", "pip", "install", "ctransformers", "--no-binary", "ctransformers"])
     subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements-amd.txt"])
     print("AMD dependencies installed!")
     if 'configure_gpu' in windows:
@@ -65,19 +73,21 @@ def install_amd(event):
 
 def configure_submenu(event):
     windows['configure_menu'] = ptg.Window(
-        ptg.Label("========== Menu =========="),
+        ptg.Label("[210 bold]========== Install =========="),
         ptg.Button("Install models", onclick=install_models),
         ptg.Button("Clear models", onclick=clear_models),
         ptg.Button("Backup", onclick=backup),
         ptg.Button("Restore", onclick=restore),
         ptg.Button("Back", onclick=lambda event: manager.remove(windows['configure_menu']))
     )
-    manager.add(windows['configure_menu'])
-    manager.focus(windows['configure_menu'])
+    with ptg.WindowManager() as manager:
+        manager.layout = _define_layout()
+        manager.add(windows['configure_menu'])
+        manager.focus(windows['configure_menu'])
 
 def install_models(event):
     windows['configure_model'] = ptg.Window(
-        ptg.Label("========== Install Models Menu =========="),
+        ptg.Label("[210 bold]========== Install =========="),
         ptg.Button("All Models", onclick=install_all_models),
         ptg.Button("All AGI Models", onclick=install_agi),
         ptg.Button("Vision", onclick=install_vision),
@@ -86,8 +96,10 @@ def install_models(event):
         ptg.Button("Yuna", onclick=install_yuna),
         ptg.Button("Back", onclick=lambda event: manager.remove(windows['configure_model']))
     )
-    manager.add(windows['configure_model'])
-    manager.focus(windows['configure_model'])
+    with ptg.WindowManager() as manager:
+        manager.layout = _define_layout()
+        manager.add(windows['configure_model'])
+        manager.focus(windows['configure_model'])
     
 def install_all_models(event):
     install_agi(event)
@@ -123,8 +135,10 @@ def clear_models(event):
         ptg.Button("Yes", onclick=clear_models_confirm),
         ptg.Button("No", onclick=lambda event: manager.remove(windows['clear_models'])),
     )
-    manager.add(windows['clear_models'])
-    manager.focus(windows['clear_models'])
+    with ptg.WindowManager() as manager:
+        manager.layout = _define_layout()
+        manager.add(windows['clear_models'])
+        manager.focus(windows['clear_models'])
 
 def clear_models_confirm(event):
     print("Clearing models...")
@@ -156,8 +170,9 @@ main_menu = ptg.Window(
     ptg.Button("Configure", onclick=configure_submenu),
     ptg.Button("Reset", onclick=clear_models),
     ptg.Button("Exit", onclick=goodbye),
-    ptg.Button("Info", onclick=info)
+    ptg.Button("Info", onclick=info),
 )
-manager = ptg.WindowManager()
-manager.add(main_menu)
-manager.run()
+with ptg.WindowManager() as manager:
+    manager.layout = _define_layout()
+    manager.add(main_menu)
+    manager.run()

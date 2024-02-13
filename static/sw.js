6ce16ffdaa2858f8f39e2bd693004b014d0a52e7
Yuki Arimo <yukiarimo@gmail.com>
2024-02-12 18:54:54 -0700

    Optimization + Performance Boost
    Minified and removed some unnecessary stuff!

--- static/sw.js
+++ static/sw.js
@@ -2,7 +2,7 @@
 
 importScripts('sw-toolbox.js');
 
-toolbox.precache(["/yuna", "/css/index.css", "fonts/kawai-font.woff", "/js/index.js", "img/yuna-ai.png", "img/yuna-full.png", "img/yuna-girl-head.png"]);
+toolbox.precache(["/yuna", "/css/index.css", "fonts/kawai-font.woff", "/js/index.js", "img/yuna-ai.png", "img/yuna-girl-head.png"]);
 
 toolbox.router.get('/images/*', toolbox.cacheFirst);
 toolbox.router.get('/*', toolbox.networkFirst, {

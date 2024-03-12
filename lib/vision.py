8eb9b3ca0986eadde85147b9255171813b6e8e90
Yuki Arimo <yukiarimo@gmail.com>
2024-03-12 11:14:49 -0600

    Prompt Termplates directory removed. Chrome extension update. Moondream model will replace captioning

--- lib/vision.py
+++ lib/vision.py
@@ -5,40 +5,20 @@ from transformers import BlipProcessor, BlipForConditionalGeneration
 from diffusers import StableDiffusionPipeline
 from PIL import Image
 from datetime import datetime
+import torch
+from PIL import Image
+from .yvision.moondream import Moondream
+from transformers import CodeGenTokenizerFast as Tokenizer
 
 if os.path.exists("static/config.json"):
     with open("static/config.json", 'r') as file:
         config = json.load(file)
 
 agi_model_dir = config["server"]["agi_model_dir"]
-processor = BlipProcessor.from_pretrained(f"{agi_model_dir}yuna-vision")
-model = BlipForConditionalGeneration.from_pretrained(f"{agi_model_dir}yuna-vision")
 
 if config["ai"]["art"] == True:
-    art = StableDiffusionPipeline.from_single_file(f'{agi_model_dir}art/{config["server"]["art_default_model"]}', safety_checker=None, load_safety_checker=None, sequence_length=256, guidance_scale=7)
+    art = StableDiffusionPipeline.from_single_file(f'{agi_model_dir}art/{config["server"]["art_default_model"]}', safety_checker=None, load_safety_checker=None, guidance_scale=7.5, noise_scale=0.05, device=config["server"]["device"])
     art.to(config["server"]["device"])
-
-def capture_image(data):
-    image_data_url = data['image']
-    image_name = data['name']
-
-    # Decode the base64 image data URL and process it as needed
-    image_data = base64.b64decode(image_data_url.split(',')[1])
-    image_path = os.path.join('static', 'img', 'call', f'{image_name}.jpg')
-    with open(image_path, 'wb') as image_file:
-        image_file.write(image_data)
-
-    img_path = f'static/img/call/{image_name}.jpg' 
-    raw_image = Image.open(img_path).convert('RGB')
-
-    # unconditional image captioning
-    inputs = processor(raw_image, return_tensors="pt")
-
-    out = model.generate(**inputs, max_length=150)
-    image_caption = str(processor.decode(out[0], skip_special_tokens=True))
-
-    # Respond with a success message
-    return image_caption
 
 def create_image(prompt):
     prompt_beautifier = ", high resolution, 4k, 8k, hd, uhd, best quality, highres, high quality"
@@ -54,3 +34,22 @@ def create_image(prompt):
     image.save(f"static/img/art/{image_name}")
 
     return image_name
+
+def capture_image(image_path, prompt=None, use_cpu=False):
+    device = torch.device("cpu") if use_cpu else (torch.device("cuda") if torch.cuda.is_available() else (torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")))
+    dtype = torch.float32 if use_cpu else (torch.float16 if torch.cuda.is_available() or torch.backends.mps.is_available() else torch.float32)
+    
+    if device != torch.device("cpu"):
+        print("Using device:", device)
+
+    model_id = "vikhyatk/moondream1"
+    tokenizer = Tokenizer.from_pretrained(model_id)
+    moondream = Moondream.from_pretrained(model_id).to(device=device, dtype=dtype)
+    moondream.eval()
+
+    image = Image.open(image_path)
+    image_embeds = moondream.encode_image(image)
+
+    print(">", prompt)
+    answer = moondream.answer_question(image_embeds, prompt, tokenizer)
+    print(answer)
